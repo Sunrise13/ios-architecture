8e7ffa81caacf6a95bb56758de5766cbb66cba4f
Alex Kunitsa <alex.kunitsa@gmail.com>
2015-09-08 14:43:11 +0300

    Initial commit

---
+++ SampleProject/Application/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  SampleProject
+//
+//  Created by Alex Kunitsa on 9/8/15.
+//  Copyright (c) 2015 Techmagic. All rights reserved.
+//
+
+import UIKit
+
+struct Constants {
+    
+    struct Color {
+        static let tableViewBorderColor = UIColor(red: 246/255, green: 246/255, blue: 246/255, alpha: 1)
+    }
+}
---
+++ SampleProject/Controllers/Sample/SampleVC.swift
@@ -0,0 +1,24 @@
+//
+//  SampleVC.swift
+//  SampleProject
+//
+//  Created by Alex Kunitsa on 9/8/15.
+//  Copyright (c) 2015 Techmagic. All rights reserved.
+//
+
+import UIKit
+
+class SampleVC: UIViewController {
+    
+    let sampleVM = SampleVM()
+
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
